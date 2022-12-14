1604892ff470970684d95dadd184b89b9f5a5a20
Garth Braithwaite <garthdb@gmail.com>
2022-12-13 20:26:46 -0700

    test: schema-formatter wip

--- test/schema-formatter.test.js
+++ test/schema-formatter.test.js
@@ -1,3 +1,11 @@
-test("Drover should match light variable values", () => {
-    expect(true).toBe(true);
-})
+const schemaFormatter = require("../lib/schema-formatter.js");
+const { readFile } = require("fs").promises;
+
+const readJSON = async (filePath) =>
+  JSON.parse(await readFile(filePath, "utf8"));
+
+test("Schema formatter", async () => {
+  const pickerSchema = await readJSON("schema/components/picker.json");
+  const pickerFixture = await readJSON("test/fixtures/picker.json");
+  expect(schemaFormatter(pickerSchema)).toBe(true);
+});
